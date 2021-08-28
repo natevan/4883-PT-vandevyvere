1dd9cecdc1b2407f4d26f1cc198498222cc3a9d6
natevan <nathanvandevyvere@gmail.com>
2021-08-28 16:32:09 -0500

    Update main.cpp

--- Assignments/P10055/main.cpp
+++ Assignments/P10055/main.cpp
@@ -1,1 +1,30 @@
+/**
+* Nathan Van De Vyvere
+* 4883 Programming Techniques
+* 08/31/2021
+*
+* Hashmat the Brave - 10055
+*/
+#include <iostream>
 
+#define endl "\n"
+
+using namespace std;
+
+int main() {
+    long int B = 0, A = 0;
+    
+    while (cin >> A >> B) {
+
+        B-=A;
+
+        if (B<0){
+          B*=-1;
+        }
+
+        cout << B << endl;
+
+
+    }
+    return 0;
+}
